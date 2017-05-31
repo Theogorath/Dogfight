566ca0572d9c7d865da12dcfb2dfbd45bfb75ef8
baelishSir <sylvain.lerest@viacesi.fr>
2017-05-31 16:19:42 +0200

    entre

--- src/jpu2017/dogfight/model/DogfightModel.java
+++ src/jpu2017/dogfight/model/DogfightModel.java
@@ -3,9 +3,8 @@ package jpu2017.dogfight.model;
 import java.util.Observable;
 
 public class DogfightModel extends Observable implements IDogfightModel{
-	
 	public DogfightModel(){
-		
+		a
 	}
 	public IArea getArea(){
 		
