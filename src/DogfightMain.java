e30bf1c97784c422f38fef587343b760e07bd8ff
Theogorath <theo.drezenugurlu@viacesi.fr>
2017-06-02 15:20:06 +0200

    modification of DogfightController and UserOrder

--- src/DogfightMain.java
+++ src/DogfightMain.java
@@ -1,8 +1,15 @@
+import jpu2017.dogfight.controller.DogfightController;
+import jpu2017.dogfight.model.DogfightModel;
+import jpu2017.dogfight.view.DogfightView;
 
 public class DogfightMain {
 
 	public static void main(String[] args) {
-		
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView);
+		dogfightController.play();
 		
 	}
 	
--- src/jpu2017/dogfight/controller/DogfightController.java
+++ src/jpu2017/dogfight/controller/DogfightController.java
@@ -3,16 +3,13 @@ package jpu2017.dogfight.controller;
 import jpu2017.dogfight.model.IDogfightModel;
 import jpu2017.dogfight.view.IViewSystem;
 
-public class DogfightController {
+public class DogfightController implements IOrderPerformer {
 	private static int TIME_SLEEP = 30;
 	private IViewSystem viewSystem;
 	private IDogfightModel dogfightModel;
 	
 	public DogfightController(IDogfightModel dogfightModel){
 		this.dogfightModel = dogfightModel;
-		
-	}
-	public void orderPerform(UserOrder userOrder){
 		
 	}
 	public void play(){
@@ -26,6 +23,11 @@ public class DogfightController {
 		
 	}
 	private void gameLoop(){
+		
+	}
+	@Override
+	public void orderPerformer(UserOrder userOrder) {
+		 UserOrder userOrder1 = userOrder;
 		
 	}
 	
